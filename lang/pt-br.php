cc06d90af6ed305f14845ed2eead32f3de3a904c
Carlos Daniel <carlosdanieldss.cd@gmail.com>
2020-05-29 18:09:05 -0300

    changing text to i18n file

--- lang/pt-br.php
+++ lang/pt-br.php
@@ -12,5 +12,42 @@ return [
         'h2-heading' => 'Marketing Automation Will Bring More Qualified Leads',
         'card-title' => 'Lists Builder',
         'p-card' => 'It is very easy to start creating email lists for your marketing actions. Just create your Tivo account',
+        'card-title_2' => 'Campaign Tracker',
+        'p-card_2' => "Campaigns is a feature we've developed since the beginning because it's at the core of Tivo's functionalities",
+        'card-title_3' => 'Analytics Tool',
+        'p-card_3' => 'Tivo collects customer data in order to help you analyse different situations and take required
+              action',
+    ],
+    'navbar' => [
+
+        'nav-link' => 'Início',
+        'nav-link_2' => 'Recursos',
+        'nav-link_3' => 'Detalhes',
+        'nav-link_4' => 'Valores',
+        'btn-outline-sm' => 'Entrar',
+
+    ],
+    'details' => [
+        'd-title' => 'Now Is The Time To Upgrade Your Marketing Solution',
+        'd-text' => "Target the right customers for your business with the help of Tivo's patented segmentation technology and deploy efficient marketing campaigns. Keep your customers happy and loyal.",
+        'media-body' => 'Understand customers and meet their requirements',
+        'media-body_2' => "Targeted client base with Tivo's efficient technology",
+        'btn-solid-reg' => 'SIGN UP',
+        'p-heading' => 'What better way to show off Tivo marketing automation saas app than presenting you some great situations of each module and tool available to users in a video',
+    ],
+
+    'features' => [
+
+        'above-heading' => 'FEATURES',
+        'h2-heading' => 'Marketing Automation',
+        'p-heading' => 'Take your business strategy to the next level and automatize your marketing tasks to save time for product  development. Tivo can provide results in less than 2 weeks',
+        'nav-link' => 'List Builder',
+        'nav-link_2' => 'Campaigns',
+        'nav-link_3' => 'List Builder',
+
+    ],
+
+    'footer' => [
+
     ],
 ];
