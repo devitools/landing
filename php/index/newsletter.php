a0c6d873564c299b6e93dff12c4ad63fb6c044fb
Carlos Daniel <carlosdanieldss.cd@gmail.com>
2020-06-03 17:42:44 -0300

    princing,newsletter add i18n

--- php/index/newsletter.php
+++ php/index/newsletter.php
@@ -3,96 +3,99 @@
   <div class="container">
     <div class="row">
       <div class="col-lg-12">
-        <div class="text-container">
-          <div class="above-heading">NEWSLETTER</div>
-          <h2>Stay Updated With The Latest News To Get More Qualified Leads</h2>
+	<div class="text-container">
+	  <div class="above-heading"><?php t('newsletter.above-heading')?></div>
+	  <h2 class="news-subtitle"><?php t('newsletter.news-subtitle')?></h2>
 
-          <!-- Newsletter Form -->
-          <form
-            id="newsletterForm"
-            data-toggle="validator"
-            data-focus="false"
-          >
-            <div class="form-group">
-              <input
-                type="email"
-                class="form-control-input"
-                id="nemail"
-                required
-              >
-              <label
-                class="label-control"
-                for="nemail"
-              >Email
-              </label>
-              <div class="help-block with-errors"></div>
-            </div>
-            <div class="form-group checkbox">
-              <input
-                type="checkbox"
-                id="nterms"
-                value="Agreed-to-Terms"
-                required
-              >
-              I've read and agree to Tivo's written
-              <a href="privacy-policy.html">Privacy Policy</a>
-              and
-              <a href="terms-conditions.html">Terms Conditions</a>
-              <div class="help-block with-errors"></div>
-            </div>
-            <div class="form-group">
-              <button
-                type="submit"
-                class="form-control-submit-button"
-              >SUBSCRIBE
-              </button>
-            </div>
-            <div class="form-message">
-              <div
-                id="nmsgSubmit"
-                class="h3 text-center hidden"
-              ></div>
-            </div>
-          </form>
-          <!-- end of newsletter form -->
+	  <!-- Newsletter Form -->
+	  <form
+	    id="newsletterForm"
+	    data-toggle="validator"
+	    data-focus="false"
+	  >
+	    <div class="form-group">
+	      <input
+		type="email"
+		class="form-control-input"
+		id="nemail"
+		required
+	      >
+	      <label
+		class="label-control"
+		for="nemail"
+	      ><?php t('newsletter.label-control')?>
+	      </label>
+	      <div class="help-block with-errors"></div>
+	    </div>
+	    <div class="form-group checkbox">
+	      <input
+		type="checkbox"
+		id="nterms"
+		value="Agreed-to-Terms"
+		required
+	      >
+	      <p class="p-newsletter"><?php t('newsletter.p-newsletter')?> </p>
 
-        </div> <!-- end of text-container -->
+	      <a class="privacy-policy-link" href="privacy-policy.html"><?php t('newsletter.privacy-policy-link')?></a>
+
+	      <p class="p-newsletter_2"><?php t('newsletter.p-newsletter_2')?></p>
+
+	      <a class="terms-conditions-link" href="terms-conditions.html"><?php t('newsletter.terms-conditions-link')?></a>
+
+	    <div class="help-block with-errors"></div>
+	    </div>
+	    <div class="form-group">
+	      <button
+		type="submit"
+		class="form-control-submit-button"
+	      ><?php t('newsletter.form-control-submit-button')?></button>
+	    </div>
+	    <div class="form-message">
+	      <div
+		id="nmsgSubmit"
+		class="h3 text-center hidden"
+	      ></div>
+	    </div>
+	  </form>
+	  <!-- end of newsletter form -->
+
+	</div> <!-- end of text-container -->
       </div> <!-- end of col -->
     </div> <!-- end of row -->
     <div class="row">
       <div class="col-lg-12">
-        <div class="icon-container">
-                        <span class="fa-stack">
-                            <a href="#your-link">
-                                <i class="fas fa-circle fa-stack-2x"></i>
-                                <i class="fab fa-facebook-f fa-stack-1x"></i>
-                            </a>
-                        </span>
-          <span class="fa-stack">
-                            <a href="#your-link">
-                                <i class="fas fa-circle fa-stack-2x"></i>
-                                <i class="fab fa-twitter fa-stack-1x"></i>
-                            </a>
-                        </span>
-          <span class="fa-stack">
-                            <a href="#your-link">
-                                <i class="fas fa-circle fa-stack-2x"></i>
-                                <i class="fab fa-pinterest-p fa-stack-1x"></i>
-                            </a>
-                        </span>
-          <span class="fa-stack">
-                            <a href="#your-link">
-                                <i class="fas fa-circle fa-stack-2x"></i>
-                                <i class="fab fa-instagram fa-stack-1x"></i>
-                            </a>
-                        </span>
-          <span class="fa-stack">
-                            <a href="#your-link">
-                                <i class="fas fa-circle fa-stack-2x"></i>
-                                <i class="fab fa-linkedin-in fa-stack-1x"></i>
-                            </a>
-                        </span>
-        </div> <!-- end of col -->
+	<div class="icon-container">
+			<span class="fa-stack">
+			    <a href="#your-link">
+				<i class="fas fa-circle fa-stack-2x"></i>
+				<i class="fab fa-facebook-f fa-stack-1x"></i>
+			    </a>
+			</span>
+	  <span class="fa-stack">
+			    <a href="#your-link">
+				<i class="fas fa-circle fa-stack-2x"></i>
+				<i class="fab fa-twitter fa-stack-1x"></i>
+			    </a>
+			</span>
+	  <span class="fa-stack">
+			    <a href="#your-link">
+				<i class="fas fa-circle fa-stack-2x"></i>
+				<i class="fab fa-pinterest-p fa-stack-1x"></i>
+			    </a>
+			</span>
+	  <span class="fa-stack">
+			    <a href="#your-link">
+				<i class="fas fa-circle fa-stack-2x"></i>
+				<i class="fab fa-instagram fa-stack-1x"></i>
+			    </a>
+			</span>
+	  <span class="fa-stack">
+			    <a href="#your-link">
+				<i class="fas fa-circle fa-stack-2x"></i>
+				<i class="fab fa-linkedin-in fa-stack-1x"></i>
+			    </a>
+			</span>
+	</div> <!-- end of col -->
       </div> <!-- end of col -->
     </div> <!-- end of row -->
   </div> <!-- end of container -->
--- php/index/pricing.php
+++ php/index/pricing.php
@@ -4,140 +4,159 @@
     class="cards-2"
 >
     <div class="container">
-        <div class="row">
-            <div class="col-lg-12">
-                <div class="above-heading">PRICING</div>
-                <h2 class="h2-heading">Pricing Options Table</h2>
-            </div> <!-- end of col -->
-        </div> <!-- end of row -->
-        <div class="row">
-            <div class="col-lg-12">
-
-                <!-- Card-->
-                <div class="card">
-                    <div class="card-body">
-                        <div class="card-title">BASIC</div>
-                        <div class="price"><span class="currency">$</span><span class="value">Free</span></div>
-                        <div class="frequency">14 days trial</div>
-                        <div class="divider"></div>
-                        <ul class="list-unstyled li-space-lg">
-                            <li class="media">
-                                <i class="fas fa-check"></i>
-                                <div class="media-body">Email Marketing Module</div>
-                            </li>
-                            <li class="media">
-                                <i class="fas fa-check"></i>
-                                <div class="media-body">User Control Management</div>
-                            </li>
-                            <li class="media">
-                                <i class="fas fa-times"></i>
-                                <div class="media-body">List Building And Cleaning</div>
-                            </li>
-                            <li class="media">
-                                <i class="fas fa-times"></i>
-                                <div class="media-body">Collected Data Reports</div>
-                            </li>
-                            <li class="media">
-                                <i class="fas fa-times"></i>
-                                <div class="media-body">Planning And Evaluation</div>
-                            </li>
-                        </ul>
-                        <div class="button-wrapper">
-                            <a
-                                class="btn-solid-reg page-scroll"
-                                href="sign-up.html"
-                            >SIGN UP
-                            </a>
-                        </div>
-                    </div>
-                </div> <!-- end of card -->
-                <!-- end of card -->
-
-                <!-- Card-->
-                <div class="card">
-                    <div class="card-body">
-                        <div class="card-title">ADVANCED</div>
-                        <div class="price"><span class="currency">$</span><span class="value">29.99</span></div>
-                        <div class="frequency">monthly</div>
-                        <div class="divider"></div>
-                        <ul class="list-unstyled li-space-lg">
-                            <li class="media">
-                                <i class="fas fa-check"></i>
-                                <div class="media-body">Email Marketing Module</div>
-                            </li>
-                            <li class="media">
-                                <i class="fas fa-check"></i>
-                                <div class="media-body">User Control Management</div>
-                            </li>
-                            <li class="media">
-                                <i class="fas fa-check"></i>
-                                <div class="media-body">List Building And Cleaning</div>
-                            </li>
-                            <li class="media">
-                                <i class="fas fa-times"></i>
-                                <div class="media-body">Collected Data Reports</div>
-                            </li>
-                            <li class="media">
-                                <i class="fas fa-times"></i>
-                                <div class="media-body">Planning And Evaluation</div>
-                            </li>
-                        </ul>
-                        <div class="button-wrapper">
-                            <a
-                                class="btn-solid-reg page-scroll"
-                                href="sign-up.html"
-                            >SIGN UP
-                            </a>
-                        </div>
-                    </div>
-                </div> <!-- end of card -->
-                <!-- end of card -->
-
-                <!-- Card-->
-                <div class="card">
-                    <!--<div class="label">
-                        <p class="best-value">Best Value</p>
-                    </div> -->
-                    <div class="card-body">
-                        <div class="card-title">COMPLETE</div>
-                        <div class="price"><span class="currency">$</span><span class="value">39.99</span></div>
-                        <div class="frequency">monthly</div>
-                        <div class="divider"></div>
-                        <ul class="list-unstyled li-space-lg">
-                            <li class="media">
-                                <i class="fas fa-check"></i>
-                                <div class="media-body">Email Marketing Module</div>
-                            </li>
-                            <li class="media">
-                                <i class="fas fa-check"></i>
-                                <div class="media-body">User Control Management</div>
-                            </li>
-                            <li class="media">
-                                <i class="fas fa-check"></i>
-                                <div class="media-body">List Building And Cleaning</div>
-                            </li>
-                            <li class="media">
-                                <i class="fas fa-check"></i>
-                                <div class="media-body">Collected Data Reports</div>
-                            </li>
-                            <li class="media">
-                                <i class="fas fa-check"></i>
-                                <div class="media-body">Planning And Evaluation</div>
-                            </li>
-                        </ul>
-                        <div class="button-wrapper">
-                            <a
-                                class="btn-solid-reg page-scroll"
-                                href="sign-up.html"
-                            >SIGN UP
-                            </a>
-                        </div>
-                    </div>
-                </div> <!-- end of card -->
-                <!-- end of card -->
-
-            </div> <!-- end of col -->
-        </div> <!-- end of row -->
+	<div class="row">
+	    <div class="col-lg-12">
+		<div class="above-heading"><?php t('pricing.above-heading')?>
+</div>
+		<h2 class="h2-heading"><?php t('pricing.h2-heading')?>
+</h2>
+	    </div> <!-- end of col -->
+	</div> <!-- end of row -->
+	<div class="row">
+	    <div class="col-lg-12">
+		<!-- Card-->
+		<div class="card">
+		    <div class="card-body">
+			<div class="card-title"><?php t('pricing.card-title')?>
+</div>
+			<div class="price"><span class="currency"><?php t('pricing.currency')?>
+</span><span class="value"><?php t('pricing.value')?>
+</span></div>
+			<div class="frequency"><?php t('pricing.frequency')?>
+</div>
+			<div class="divider"></div>
+			<ul class="list-unstyled li-space-lg">
+			    <li class="media">
+				<i class="fas fa-check"></i>
+				<div class="media-body"><?php t('pricing.media-body')?>
+</div>
+			    </li>
+			    <li class="media">
+				<i class="fas fa-check"></i>
+				<div class="media-body_2"><?php t('pricing.media-body_2')?>
+</div>
+			    </li>
+			    <li class="media">
+				<i class="fas fa-times"></i>
+				<div class="media-body_3"><?php t('pricing.media-body_3')?>
+</div>
+			    </li>
+			    <li class="media">
+				<i class="fas fa-times"></i>
+				<div class="media-body_4"><?php t('pricing.media-body_4')?>
+</div>
+			    </li>
+			    <li class="media">
+				<i class="fas fa-times"></i>
+				<div class="media-body_5"><?php t('pricing.media-body_5')?>
+</div>
+			    </li>
+			</ul>
+			<div class="button-wrapper">
+			    <a
+				class="btn-solid-reg page-scroll"
+				href="sign-up.html"
+			    ><?php t('pricing.btn-solid-reg')?>
+			    </a>
+			</div>
+		    </div>
+		</div> <!-- end of card -->
+		<!-- end of card -->
+		<!-- Card-->
+		<div class="card">
+		    <div class="card-body">
+			<div class="card-title_2"><?php t('pricing.card-title_2')?></div>
+			<div class="price"><span class="currency"><?php t('pricing.currency')?></span><span class="value_2"><?php t('pricing.value_2')?></span></div>
+			<div class="frequency-monthly"><?php t('pricing.frequency-monthly')?></div>
+			<div class="divider"></div>
+			<ul class="list-unstyled li-space-lg">
+			    <li class="media">
+				<i class="fas fa-check"></i>
+				<div class="media-body"><?php t('pricing.media-body')?>
+</div>
+			    </li>
+			    <li class="media">
+				<i class="fas fa-check"></i>
+				<div class="media-body_2"><?php t('pricing.media-body_2')?>
+</div>
+			    </li>
+			    <li class="media">
+				<i class="fas fa-times"></i>
+				<div class="media-body_3"><?php t('pricing.media-body_3')?>
+</div>
+			    </li>
+			    <li class="media">
+				<i class="fas fa-times"></i>
+				<div class="media-body_4"><?php t('pricing.media-body_4')?>
+</div>
+			    </li>
+			    <li class="media">
+				<i class="fas fa-times"></i>
+				<div class="media-body_5"><?php t('pricing.media-body_5')?>
+</div>
+			    </li>
+			    <li class="media">
+			    </li>
+			</ul>
+			<div class="button-wrapper">
+			    <a
+				class="btn-solid-reg page-scroll"
+				href="sign-up.html"
+			    ><?php t('pricing.btn-solid-reg')?>
+			    </a>
+			</div>
+		    </div>
+		</div> <!-- end of card -->
+		<!-- end of card -->
+		<!-- Card-->
+		<div class="card">
+		    <!--<div class="label">
+			<p class="best-value">Best Value</p>
+		    </div> -->
+		    <div class="card-body">
+			<div class="card-title_3"><?php t('pricing.card-title_3')?></div>
+			<div class="price"><span class="currency"><?php t('pricing.currency')?></span><span class="value_3"><?php t('pricing.value_3')?></span></div>
+			<div class="frequency-monthly"><?php t('pricing.frequency-monthly')?></div>
+			<div class="divider"></div>
+			<ul class="list-unstyled li-space-lg">
+			    <li class="media">
+				<i class="fas fa-check"></i>
+				<div class="media-body"><?php t('pricing.media-body')?>
+</div>
+			    </li>
+			    <li class="media">
+				<i class="fas fa-check"></i>
+				<div class="media-body_2"><?php t('pricing.media-body_2')?>
+</div>
+			    </li>
+			    <li class="media">
+				<i class="fas fa-times"></i>
+				<div class="media-body_3"><?php t('pricing.media-body_3')?>
+</div>
+			    </li>
+			    <li class="media">
+				<i class="fas fa-times"></i>
+				<div class="media-body_4"><?php t('pricing.media-body_4')?>
+</div>
+			    </li>
+			    <li class="media">
+				<i class="fas fa-times"></i>
+				<div class="media-body_5"><?php t('pricing.media-body_5')?>
+</div>
+			    </li>
+			</ul>
+			<div class="button-wrapper">
+			    <a
+				class="btn-solid-reg page-scroll"
+				href="sign-up.html"
+			    ><?php t('pricing.btn-solid-reg')?>
+			    </a>
+			</div>
+		    </div>
+		</div> <!-- end of card -->
+		<!-- end of card -->
+	    </div> <!-- end of col -->
+	</div> <!-- end of row -->
     </div> <!-- end of container -->
 </div> <!-- end of cards-2 -->
 <!-- end of pricing -->
